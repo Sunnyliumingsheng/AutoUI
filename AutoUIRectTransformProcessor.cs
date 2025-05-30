eaee0c7a557319619a6f1bfd0450e8e5ccf7de23
yangxiaoshuai <yangming01@tuyoogame.com>
2025-05-30 15:29:05 +0800

    修复了对anchor的bug

--- AutoUIRectTransformProcessor.cs
+++ AutoUIRectTransformProcessor.cs
@@ -12,6 +12,10 @@ namespace Assets.Scripts.Tools.Editor.AutoUI
         {
             rectTransform.anchoredPosition = layerRectTransformData.anchoredPosition.ToVector2();
             rectTransform.sizeDelta = layerRectTransformData.sizeDelta.ToVector2();
+            if (layerRectTransformData.anchor == null)
+            {
+                LogUtil.LogError("layerRectTransformData.anchor is null,请检查JSON搜索null,非常非常有可能是美术在图层命名的时候使用到/符号导致");
+            }
             rectTransform.anchorMin = layerRectTransformData.anchor[0].ToVector2();
             rectTransform.anchorMax = layerRectTransformData.anchor[1].ToVector2();
             rectTransform.pivot = layerRectTransformData.pivot.ToVector2();
